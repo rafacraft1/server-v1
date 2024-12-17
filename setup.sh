cf47e6bd79a0df201b3c8cb90755809573d90c34
Indra Agustian <indraagustian1@gmail.com>
2024-12-17 17:01:06 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Skrip Instalasi Apache2, PHP8, dan Composer untuk Ubuntu Server
+# Skrip Interaktif Instalasi Apache2, PHP8, Composer, dan Database untuk Ubuntu Server
 # Penulis: Indra Agustian
 # Tanggal: $(date)
 
@@ -9,6 +9,18 @@ echo_message() {
     echo "============================================"
     echo "$1"
     echo "============================================"
+}
+
+# Fungsi untuk menampilkan progres
+show_progress() {
+    local duration=$1
+    local message=$2
+    echo -n "$message"
+    for i in $(seq 1 $duration); do
+        echo -n "."
+        sleep 1
+    done
+    echo " Done!"
 }
 
 # Fungsi untuk pemeriksaan awal
@@ -33,6 +45,7 @@ pre_check() {
 # Fungsi untuk instalasi Apache2
 install_apache2() {
     echo_message "Menginstal Apache2..."
+    show_progress 3 "Proses instalasi Apache2 dimulai"
     apt update -y && apt install apache2 -y
     systemctl start apache2
     systemctl enable apache2
@@ -42,11 +55,13 @@ install_apache2() {
 # Fungsi untuk instalasi PHP8 dan modul
 install_php8() {
     echo_message "Menambahkan repositori PHP8..."
+    show_progress 2 "Menambahkan repositori PHP8"
     apt install software-properties-common -y
     add-apt-repository ppa:ondrej/php -y
     apt update -y
 
     echo_message "Menginstal PHP8 dan modul tambahan..."
+    show_progress 5 "Proses instalasi PHP8 dimulai"
     apt install php8.2 libapache2-mod-php8.2 php8.2-cli php8.2-fpm \
         php8.2-mysql php8.2-xml php8.2-curl php8.2-gd \
         php8.2-mbstring php8.2-zip php8.2-bcmath php8.2-pgsql -y
@@ -66,6 +81,7 @@ install_php8() {
 # Fungsi untuk instalasi Composer
 install_composer() {
     echo_message "Menginstal Composer..."
+    show_progress 3 "Proses instalasi Composer dimulai"
 
     # Unduh installer Composer
     curl -sS https://getcomposer.org/installer | php8.2 --install-dir=/usr/local/bin --filename=composer
@@ -80,6 +96,50 @@ install_composer() {
     fi
 }
 
+# Fungsi untuk instalasi MySQL
+install_mysql() {
+    echo_message "Menginstal MySQL..."
+    show_progress 4 "Proses instalasi MySQL dimulai"
+
+    # Instalasi MySQL Server
+    apt update -y
+    apt install mysql-server -y
+
+    # Menjalankan dan mengaktifkan MySQL
+    systemctl start mysql
+    systemctl enable mysql
+
+    # Mengamankan instalasi MySQL
+    mysql_secure_installation
+
+    echo "MySQL berhasil diinstal dan dijalankan."
+
+    # Menginstal ekstensi MySQL untuk PHP8
+    echo_message "Menginstal ekstensi MySQL untuk PHP8..."
+    apt install php8.2-mysql -y
+
+    # Restart Apache agar ekstensi PHP MySQL aktif
+    systemctl restart apache2
+
+    echo "Ekstensi MySQL untuk PHP8 berhasil diinstal."
+}
+
+# Fungsi untuk instalasi PostgreSQL
+install_postgresql() {
+    echo_message "Menginstal PostgreSQL..."
+    show_progress 4 "Proses instalasi PostgreSQL dimulai"
+
+    # Instalasi PostgreSQL Server
+    apt update -y
+    apt install postgresql postgresql-contrib -y
+
+    # Menjalankan dan mengaktifkan PostgreSQL
+    systemctl start postgresql
+    systemctl enable postgresql
+
+    echo "PostgreSQL berhasil diinstal dan dijalankan."
+}
+
 # Fungsi membuat file info.php
 create_info_php() {
     echo_message "Membuat file info.php untuk pengujian PHP..."
@@ -88,47 +148,73 @@ create_info_php() {
     echo "Akses melalui: http://<alamat-ip-server>/info.php"
 }
 
-# Fungsi utama untuk menu interaktif
+# Fungsi untuk menampilkan menu utama
 main_menu() {
-    echo_message "Selamat datang di Skrip Instalasi Apache2, PHP8, dan Composer Interaktif"
+    echo_message "Selamat datang di Skrip Instalasi Apache2, PHP8, Composer, dan Database Interaktif"
 
     # Pemeriksaan awal
     pre_check
 
-    # Tanya instalasi Apache2
-    read -p "Apakah Anda ingin menginstal Apache2? (y/n): " install_apache
-    if [[ "$install_apache" == "y" || "$install_apache" == "Y" ]]; then
-        install_apache2
-    else
-        echo "Lewati instalasi Apache2."
-    fi
+    while true; do
+        # Menampilkan pilihan menu
+        echo_message "Menu Instalasi:"
+        echo "1) Instal Apache2"
+        echo "2) Instal PHP8 dan Modul terkait"
+        echo "3) Instal Composer"
+        echo "4) Pilih dan Instal Database (MySQL/PostgreSQL)"
+        echo "5) Buat file info.php untuk pengujian PHP"
+        echo "6) Keluar"
+        read -p "Pilih opsi (1-6): " option
 
-    # Tanya instalasi PHP8
-    read -p "Apakah Anda ingin menginstal PHP8 beserta modulnya? (y/n): " install_php
-    if [[ "$install_php" == "y" || "$install_php" == "Y" ]]; then
-        install_php8
-    else
-        echo "Lewati instalasi PHP8."
-    fi
+        case $option in
+            1)
+                echo_message "Anda memilih untuk menginstal Apache2."
+                install_apache2
+                ;;
+            2)
+                echo_message "Anda memilih untuk menginstal PHP8 beserta modulnya."
+                install_php8
+                ;;
+            3)
+                echo_message "Anda memilih untuk menginstal Composer."
+                install_composer
+                ;;
+            4)
+                echo_message "Anda memilih untuk memilih dan menginstal database."
+                # Pilih MySQL atau PostgreSQL
+                read -p "Pilih database untuk diinstal (1) MySQL (2) PostgreSQL: " db_choice
+                case $db_choice in
+                    1)
+                        install_mysql
+                        ;;
+                    2)
+                        install_postgresql
+                        ;;
+                    *)
+                        echo "Pilihan tidak valid. Kembali ke menu utama."
+                        ;;
+                esac
+                ;;
+            5)
+                echo_message "Anda memilih untuk membuat file info.php."
+                create_info_php
+                ;;
+            6)
+                echo_message "Terima kasih telah menggunakan skrip ini. Keluar..."
+                exit 0
+                ;;
+            *)
+                echo "Pilihan tidak valid. Silakan pilih antara 1-6."
+                ;;
+        esac
 
-    # Tanya instalasi Composer
-    read -p "Apakah Anda ingin menginstal Composer? (y/n): " install_composer
-    if [[ "$install_composer" == "y" || "$install_composer" == "Y" ]]; then
-        install_composer
-    else
-        echo "Lewati instalasi Composer."
-    fi
-
-    # Tanya pembuatan file info.php
-    read -p "Apakah Anda ingin membuat file info.php untuk pengujian PHP? (y/n): " create_info
-    if [[ "$create_info" == "y" || "$create_info" == "Y" ]]; then
-        create_info_php
-    else
-        echo "Lewati pembuatan file info.php."
-    fi
-
-    echo_message "Proses instalasi selesai!"
-    echo "Jika Apache2, PHP8, dan Composer diinstal, silakan akses server melalui browser Anda."
+        # Tanyakan apakah pengguna ingin melakukan lebih banyak instalasi
+        read -p "Apakah Anda ingin melakukan instalasi lain? (y/n): " continue_choice
+        if [[ "$continue_choice" != "y" && "$continue_choice" != "Y" ]]; then
+            echo_message "Terima kasih telah menggunakan skrip ini. Keluar..."
+            exit 0
+        fi
+    done
 }
 
 # Menjalankan fungsi utama
