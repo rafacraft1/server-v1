7e0a910f2c3cb7573e48c426646e2338a9a92d1b
Indra Agustian <indraagustian1@gmail.com>
2024-12-21 09:42:46 +0700

    Update install_apache2.sh

--- scripts/install_apache2.sh
+++ scripts/install_apache2.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Proteksi agar hanya bisa dijalankan dari setup.sh
 if [ "$IS_SETUP_SH" != "$(echo 'secure_hash_value' | sha256sum | awk '{print $1}')" ]; then
   echo -e "\033[0;31m[ERROR] File ini hanya bisa dijalankan dari setup.sh.\033[0m"
   exit 1
 fi
 
-# Fungsi menginstal Apache2
 install_apache2() {
-  echo -e "${BLUE}Menginstal Apache2...${NC}"
+  print_status "${BLUE}Menginstal Apache2...${NC}"
   check_internet || return
-  sudo apt install apache2 -y
+  sudo apt install apache2 -y &> /dev/null
   if [ $? -eq 0 ]; then
     print_success "Apache2 berhasil diinstal."
   else
-    print_error "Gagal menginstal Apache2. Mencoba memperbaiki masalah..."
+    print_status "${RED}Gagal menginstal Apache2. Mencoba memperbaiki...${NC}"
     attempt_fix
-    sudo apt install apache2 -y
+    sudo apt install apache2 -y &> /dev/null
     if [ $? -eq 0 ]; then
       print_success "Apache2 berhasil diinstal setelah perbaikan."
     else
